b443abc26fec6e3a3b1e6a5dd181caf062257eaa
Aydin Pramasta <aydin.pram@gmail.com>
2023-06-06 16:29:09 +0700

    setup + navbar

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Plus Jakarta Sans", "sans-serif"],
+      logo: ["Proxima Nova", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
